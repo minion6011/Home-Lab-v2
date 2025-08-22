68bf70059bcc251b369e41bd1220acf36fe08779
Minion6011 <mvittorio29@gmail.com>
2025-08-22 10:23:15 +0200

    Update main.py

--- code/main.py
+++ code/main.py
@@ -32,7 +32,11 @@ def index():
 @app.route('/manifest.json')
 def serve_manifest():
     return send_file('manifest.json', mimetype='application/manifest+json')
-	
+
+@app.route('/sw.js')
+def serve_sw():
+    return send_file('sw.js', mimetype='application/javascript')
+
 # - API
 @app.route('/stats', methods=['POST'])
 def system_stats():
